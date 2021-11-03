7f4d14702d21c6237bba234296094385960a07d7
yasamin-niknam <41532584+yasamin-niknam@users.noreply.github.com>
2021-11-03 12:48:17 -0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,89 @@
+import numpy as np
+import tensorflow as tf
+
+
+def load(image_file):
+  # Read and decode an image file to a uint8 tensor
+  image = tf.io.read_file(image_file)
+  image = tf.image.decode_jpeg(image)
+
+  # Convert both images to float32 tensors
+  input_image = tf.cast(image, tf.float32)
+  real_image = input_image
+
+  return input_image, real_image
+
+
+def resize(input_image, real_image, height, width):
+  input_image = tf.image.resize(input_image, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  real_image = tf.image.resize(real_image, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+  return input_image, real_image
+
+
+def random_crop(input_image, real_image):
+  IMG_WIDTH = 256
+  IMG_HEIGHT = 256
+  stacked_image = tf.stack([input_image, real_image], axis=0)
+  cropped_image = tf.image.random_crop(
+      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+
+  return cropped_image[0], cropped_image[1]
+
+
+# Normalizing the images to [-1, 1]
+def normalize(input_image, real_image):
+  input_image = (input_image / 127.5) - 1
+  real_image = (real_image / 127.5) - 1
+
+  return input_image, real_image
+
+
+def occlude(image):
+  occlusion_size = np.random.randint(30, 50)
+  mask = np.zeros((occlusion_size, occlusion_size, 3))
+  random = np.random.randint(0, 200)
+  image[random:occlusion_size + random, random:occlusion_size + random, :] = mask
+  return image
+
+
+@tf.function()
+def random_jitter(input_image, real_image):
+
+  # Resizing to 286x286
+  input_image, real_image = resize(input_image, real_image, 286, 286)
+
+  # Random cropping back to 256x256
+  input_image, real_image = random_crop(input_image, real_image)
+
+  if tf.random.uniform(()) > 0.5:
+    # Random mirroring
+    input_image = tf.image.flip_left_right(input_image)
+    real_image = tf.image.flip_left_right(real_image)
+
+  return input_image, real_image
+
+
+def load_image_train(image_file):
+
+  input_image, real_image = load(image_file)
+  print(input_image.shape)
+  print(real_image.shape)
+  input_image, real_image = random_jitter(input_image, real_image)
+  input_image, real_image = normalize(input_image, real_image)
+
+  return input_image, real_image
+
+
+def load_image_test(image_file):
+  IMG_WIDTH = 256
+  IMG_HEIGHT = 256
+  input_image, real_image = load(image_file)
+  input_image = occlude(input_image)
+  input_image, real_image = resize(input_image, real_image, IMG_HEIGHT, IMG_WIDTH)
+  input_image, real_image = normalize(input_image, real_image)
+
+  return input_image, real_image
+
---
+++ main.py
@@ -0,0 +1,100 @@
+import os
+import time
+import datetime
+import tensorflow as tf
+from IPython import display
+
+from model import *
+from functions import *
+
+
+@tf.function
+def train_step(input_image, target, step):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator(input_image, training=True)
+
+        disc_real_output = discriminator([input_image, target], training=True)
+        disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss,
+                                            generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss,
+                                                 discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(generator_gradients,
+                                            generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                                discriminator.trainable_variables))
+
+    with summary_writer.as_default():
+        tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+        tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+        tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+        tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
+
+
+def fit(train_ds, steps, test_ds=None):
+
+    start = time.time()
+    print('****************INJJAAA*****')
+    for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
+        print('I COME')
+        if (step) % 1000 == 0:
+            display.clear_output(wait=True)
+
+            if step != 0:
+                print(f'Time taken for 1000 steps: {time.time()-start:.2f} sec\n')
+
+            start = time.time()
+
+            # generate_images(generator, example_input, example_target)
+            print(f"Step: {step//1000}k")
+
+        train_step(input_image, target, step)
+
+        # Training step
+        if (step+1) % 10 == 0:
+            print('.', end='', flush=True)
+
+
+        # Save (checkpoint) the model every 5k steps
+        if (step + 1) % 5000 == 0:
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+
+# The facade training set consist of 400 images
+BUFFER_SIZE = 1281167
+# The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
+BATCH_SIZE = 128
+# Each image is 256x256 in size
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+OUTPUT_CHANNELS = 3
+
+PATH = '/localscratch/yasamin.27340825.0/train'
+
+train_dataset = tf.data.Dataset.list_files(PATH + '/*.JPEG')
+train_dataset = train_dataset.map(lambda x: tf.py_function(load_image_train, [x], [tf.string]),
+                                  num_parallel_calls=tf.data.AUTOTUNE)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
+
+generator = Generator()
+discriminator = Discriminator()
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator, discriminator=discriminator)
+
+log_dir = "logs/"
+summary_writer = tf.summary.create_file_writer(
+  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+fit(train_dataset, steps=int(BUFFER_SIZE/BATCH_SIZE)*100)
