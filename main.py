6f26ea7b717162426ec104d8ba61cd2a965e775e
Yasmin Niknam <41532584+yasminniknam@users.noreply.github.com>
2021-11-09 10:17:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -94,12 +94,12 @@ train_dataset = train_dataset.batch(BATCH_SIZE)
 generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
-checkpoint_dir = './training_checkpoints'
+checkpoint_dir = '/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, discriminator_optimizer=discriminator_optimizer,
                                  generator=generator, discriminator=discriminator)
 
-log_dir = "logs/"
+log_dir = "/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/logs/"
 summary_writer = tf.summary.create_file_writer(
   log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
