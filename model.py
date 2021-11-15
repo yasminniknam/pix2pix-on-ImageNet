6b6c36faa3aa068d364186b3a09438a397a9da73
Yasmin Niknam <41532584+yasminniknam@users.noreply.github.com>
2021-11-15 11:11:25 -0500

    Update model.py

--- model.py
+++ model.py
@@ -147,7 +147,11 @@ def generate_images(model, test_input, tar):
   display_list = [test_input[0], tar[0], prediction[0]]
   title = ['Input Image', 'Ground Truth', 'Predicted Image']
   print(prediction[0])
-  im = Image.fromarray(prediction[0])
+  # input_image = (input_image / 127.5) - 1
+  prediction[0] += 1
+  prediction[0] *= 127.5
+  print(prediction[0])
+  im = Image.fromarray(prediction[0].numpy())
   im.save("your_file.jpeg")
   # for i in range(3):
   #   plt.subplot(1, 3, i+1)
