a6f463332894ba81f472150ce6a0131eb15111e2
Yasmin Niknam <41532584+yasminniknam@users.noreply.github.com>
2021-11-16 17:34:23 -0500

    Update test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, discri
                                  generator=generator, discriminator=discriminator)
 
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-addr = '/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/results/non_occluded_model'
+addr = '/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/results/non_occluded_model/output_occluded_input'
 
 # Run the trained model on a few examples from the test set
 for inp, tar, name in test_dataset:
