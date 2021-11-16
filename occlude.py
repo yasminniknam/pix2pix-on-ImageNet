479e4ce3cdaa5f35ec91b0b617090bd207b4068f
Yasmin Niknam <41532584+yasminniknam@users.noreply.github.com>
2021-11-16 18:19:37 -0500

    Update occlude.py

--- occlude.py
+++ occlude.py
@@ -23,15 +23,16 @@ imageset = datasets.ImageFolder(root=addr,  transform=img_transform)
 
 filenames= [imageset.imgs[i][0][11:-5] for i in range(len(imageset.imgs)) ]
 print(filenames)
-#dataloader = torch.utils.data.DataLoader(imageset,batch_size=1,shuffle=False, num_workers=8)
-#save_path = '/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/results/occluded_input'
+dataloader = torch.utils.data.DataLoader(imageset,batch_size=1,shuffle=False, num_workers=8)
+save_path = '/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/results/occluded_input'
 #os.makedirs(save_path, exist_ok=True)
-#for idx, (img, y) in enumerate(dataloader):
-#    if idx == 0:
-#        print(y)
-#        print(idx)
-#        print(img.shape)
-#    new_img = occlude(img)
-#    save_image(new_img, os.path.join(save_path, filenames[idx] + '.jpeg'), nrow=8, normalize=True)
+for idx, (img, y) in enumerate(dataloader):
+    if idx == 0:
+        print(y)
+        print(idx)
+        print(img.shape)
+    new_img = occlude(img)
+    print(filenames[idx], ' done!')
+    save_image(new_img, os.path.join(save_path, filenames[idx] + '.jpeg'), nrow=8, normalize=True)
     # break
 
