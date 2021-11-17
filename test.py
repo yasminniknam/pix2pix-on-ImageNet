837717028393ded5570d3bdce1e3e95c78df306a
Yasmin Niknam <41532584+yasminniknam@users.noreply.github.com>
2021-11-17 15:03:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 addr = '/home/yasamin/scratch/pix2pix/pix2pix-on-ImageNet/results/non_occluded_model/output_occluded_input'
 
 # Run the trained model on a few examples from the test set
-for inp, tar, name in test_dataset.take(2):
+for inp, tar, name in test_dataset:
     img_name = name.numpy()[0]
     img_name = img_name[6:-5]
     img_name = img_name.decode("utf-8")
